630762a0ce24a90b343d25ead8df1eda1e04adcd
notebook <martinvojtek03@gmail.com>
2025-04-23 12:48:28 +0200

    vytvoření třídy Item

---
+++ Item.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace STIN_BurzaModule
+{
+    public class Item
+    {
+        string name;
+        DateOnly date;
+        int rating; //
+        int sell;   //1 0
+    }
+}
