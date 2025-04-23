0645d998faf31407c83277d2da6fc4b79e6a3483
notebook <martinvojtek03@gmail.com>
2025-04-23 12:47:44 +0200

    vytvoření třídy Item

--- obj/Debug/net8.0/STIN_BurzaModule.AssemblyInfo.cs
+++ obj/Debug/net8.0/STIN_BurzaModule.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STIN_BurzaModule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25223a0f27d4df46a3cba16eb7851ae6c5db1b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5c47f7e539a26eb36fede013e198b7aaf38992")]
 [assembly: System.Reflection.AssemblyProductAttribute("STIN_BurzaModule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STIN_BurzaModule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
