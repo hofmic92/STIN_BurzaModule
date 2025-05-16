865e0d388687fd11c4bde7ae7214dfb9a2953037
Filip Vala <filip.vala010@gmail.com>
2025-05-16 15:09:25 +0200

    2. pokus pro testy

--- obj/Debug/net8.0/STIN_BurzaModule.AssemblyInfo.cs
+++ obj/Debug/net8.0/STIN_BurzaModule.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STIN_BurzaModule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc7375c49748df7e9b107dbfded969395e0103c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97958c4ca9136670a9a8b5cf8e6d3f82483b7bea")]
 [assembly: System.Reflection.AssemblyProductAttribute("STIN_BurzaModule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STIN_BurzaModule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
